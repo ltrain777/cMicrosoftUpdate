a901a86bc995dea329bba894f53e92a8781a4ec8
Matt Huynh <liem.tri.huynh@avanade.com>
2016-03-21 11:58:51 -0700

    Add Test for running module individually.

--- Tests/DSC.tests.ps1
+++ Tests/DSC.tests.ps1
@@ -37,9 +37,7 @@ Describe "Validate your Default WSUS Configuration" {
  
     It "Target Group Enabled" {
         $Global.TargetGroupEnabled | Should be "1"
-    }
- 
-    
+    }   
  
     It "User can access windows updates site" {
         $Global.DisableWindowsUpdateAccess  | Should be "0"
@@ -66,7 +64,7 @@ Describe "Validate your Default WSUS Configuration" {
 
 Copy-Item "$ProjectRoot\TestWSUS\localhost.mof" "$ProjectRoot\Artifacts\Test2.mof" -Force
 
-Describe "Validate your Default WSUS Configuration" {
+Describe "Validate your Default Absent WSUS Configuration" {
     
     $Global = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate
     $Configuration = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate\AU
@@ -93,9 +91,161 @@ Describe "Validate your Default WSUS Configuration" {
  
     It "Target Group Enabled removed" {
         $Global.TargetGroupEnabled | Should BeNullOrEmpty
+    }   
+ 
+    It "User can access windows updates site should be removed" {
+        $Global.DisableWindowsUpdateAccess  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Reboot for Logged on Users should be removed" {
+        $Configuration.NoAutoRebootWithLoggedOnUsers  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Update should be removed" {
+        $Configuration.NoAutoUpdate  | Should BeNullOrEmpty
+    }
+
+    It "Scheduled install day should be removed" {
+        $Configuration.ScheduledInstallDay  | Should BeNullOrEmpty
+    }
+    
+}
+
+
+. "$here\WSUSConfig.ps1" -UseWUServer "True"
+
+Copy-Item "$ProjectRoot\TestWSUS\localhost.mof" "$ProjectRoot\Artifacts\Test3.mof" -Force
+
+Describe "Validate UseWUServer module Configuration" {
+    
+    $Global = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate
+    $Configuration = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate\AU
+ 
+    It "Automatic updates enabled with WSUS Server" {
+        $Configuration.UseWUServer | Should be "1"
     }
  
+    It "WSUS Server removed" {
+        $Global.WUServer | Should BeNullOrEmpty
+    }
+
+    It "WSUS Status Server removed" {
+        $Global.WUStatusServer | Should BeNullOrEmpty
+    }
     
+    It "AU Options should be removed" {
+        $Configuration.AUOptions | Should BeNullOrEmpty
+    }
+ 
+    It "Target group should be removed" {
+        $Global.TargetGroup | Should BeNullOrEmpty
+    }
+ 
+    It "Target Group Enabled removed" {
+        $Global.TargetGroupEnabled | Should BeNullOrEmpty
+    }   
+ 
+    It "User can access windows updates site should be removed" {
+        $Global.DisableWindowsUpdateAccess  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Reboot for Logged on Users should be removed" {
+        $Configuration.NoAutoRebootWithLoggedOnUsers  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Update should be removed" {
+        $Configuration.NoAutoUpdate  | Should BeNullOrEmpty
+    }
+
+    It "Scheduled install day should be removed" {
+        $Configuration.ScheduledInstallDay  | Should BeNullOrEmpty
+    }
+    
+}
+
+. "$here\WSUSConfig.ps1" -WSUSServerEnsure "Present" -WSUSServerUrl "https://127.0.0.1:8081" 
+
+Copy-Item "$ProjectRoot\TestWSUS\localhost.mof" "$ProjectRoot\Artifacts\Test4.mof" -Force
+
+Describe "Validate WSUSServer Present Configuration" {
+    
+    $Global = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate
+    $Configuration = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate\AU
+ 
+    It "Automatic updates enabled with WSUS Server" {
+        $Configuration.UseWUServer | Should be "1"
+    }
+ 
+    It "WSUS Server setted to https://127.0.0.1:8081" {
+        $Global.WUServer | Should be "https://127.0.0.1:8081"
+    }
+
+    It "WSUS Status Server setted to https://127.0.0.1:8081" {
+        $Global.WUStatusServer | Should be "https://127.0.0.1:8081"
+    }
+    
+    It "AU Options should be removed" {
+        $Configuration.AUOptions | Should BeNullOrEmpty
+    }
+ 
+    It "Target group should be removed" {
+        $Global.TargetGroup | Should BeNullOrEmpty
+    }
+ 
+    It "Target Group Enabled removed" {
+        $Global.TargetGroupEnabled | Should BeNullOrEmpty
+    }
+ 
+    It "User can access windows updates site should be removed" {
+        $Global.DisableWindowsUpdateAccess  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Reboot for Logged on Users should be removed" {
+        $Configuration.NoAutoRebootWithLoggedOnUsers  | Should BeNullOrEmpty
+    }
+
+    It "Disable Auto Update should be removed" {
+        $Configuration.NoAutoUpdate  | Should BeNullOrEmpty
+    }
+
+    It "Scheduled install day should be removed" {
+        $Configuration.ScheduledInstallDay  | Should BeNullOrEmpty
+    }
+    
+}
+
+. "$here\WSUSConfig.ps1" -TargetGroupEnsure "Present" -TargetGroup "DMZPrivate1" 
+
+Copy-Item "$ProjectRoot\TestWSUS\localhost.mof" "$ProjectRoot\Artifacts\Test5.mof" -Force
+
+Describe "Validate WSUSServer Present Configuration" {
+    
+    $Global = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate
+    $Configuration = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\windows\WindowsUpdate\AU
+ 
+    It "Automatic updates enabled with WSUS Server" {
+        $Configuration.UseWUServer | Should be "1"
+    }
+ 
+    It "WSUS Server setted to https://127.0.0.1:8081" {
+        $Global.WUServer | Should be "https://127.0.0.1:8081"
+    }
+
+    It "WSUS Status Server setted to https://127.0.0.1:8081" {
+        $Global.WUStatusServer | Should be "https://127.0.0.1:8081"
+    }
+    
+    It "AU Options should be removed" {
+        $Configuration.AUOptions | Should BeNullOrEmpty
+    }
+ 
+    It "Target group is set to DMZ Private" {
+        $Global.TargetGroup | Should be "DMZPrivate1"
+    }
+ 
+    It "Target Group Enabled" {
+        $Global.TargetGroupEnabled | Should be "1"
+    }
  
     It "User can access windows updates site should be removed" {
         $Global.DisableWindowsUpdateAccess  | Should BeNullOrEmpty
